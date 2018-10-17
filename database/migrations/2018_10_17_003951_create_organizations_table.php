b8de84dfff5ad0e9682a81c15642496845b47dd0
skander <skander.kc@gmail.com>
2018-10-16 21:24:47 -0400

    Create database and setup docker

---
+++ database/migrations/2018_10_17_003951_create_organizations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('email');
+            $table->text('description');
+            $table->string('facebook')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('logo');
+            $table->string('cover')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizations');
+    }
+}
